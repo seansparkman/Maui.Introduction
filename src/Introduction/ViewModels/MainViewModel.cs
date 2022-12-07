62576ebc81660af20d1714bed3cf1910e4468c95
Sean Sparkman <sean.sparkman@energyworldnet.com>
2022-12-07 13:19:45 -0600

    Step 2. Create a View Model

---
+++ src/Introduction/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Introduction.ViewModels
+{
+    public sealed class MainViewModel : ObservableObject
+    {
+        private int? _count;
+        public int? Count
+        {
+            get => _count;
+            set => SetProperty(ref _count, value);
+        }
+
+        public ICommand IncrementCountCommand { get; }
+
+        public MainViewModel()
+        {
+            IncrementCountCommand = new Command(IncrementCount);
+        }
+
+        private void IncrementCount()
+        {
+            Count = Count + 1 ?? 1;
+        }
+    }
+}
