3e6ae2830cf20b506e041adc3a96015bf27aa6c4
Sean Sparkman <sean.sparkman@energyworldnet.com>
2022-12-07 16:14:51 -0600

    Use the rest service

--- src/Introduction/ViewModels/MainViewModel.cs
+++ src/Introduction/ViewModels/MainViewModel.cs
@@ -1,27 +1,45 @@
-﻿using System.Windows.Input;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Introduction.Services;
+using Refit;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Contact = Introduction.Models.Contact;
 
 namespace Introduction.ViewModels
 {
     public sealed class MainViewModel : ObservableObject
     {
-        private int? _count;
-        public int? Count
-        {
-            get => _count;
-            set => SetProperty(ref _count, value);
-        }
+        private IRandomUser UserApi { get; }
 
-        public ICommand IncrementCountCommand { get; }
+        public ICommand LoadCommand { get; }
+
+        public ObservableCollection<Contact> Contacts { get; } = new ();
 
         public MainViewModel()
         {
-            IncrementCountCommand = new Command(IncrementCount);
+            UserApi = RestService.For<IRandomUser>(
+                "https://randomuser.me",
+                new RefitSettings(new NewtonsoftJsonContentSerializer()));
+
+            LoadCommand = new AsyncRelayCommand(Load);
         }
 
-        private void IncrementCount()
+        private async Task Load()
         {
-            Count = Count + 1 ?? 1;
+            try
+            {
+                var response = await UserApi.GetUsers("northdallas");
+                
+                response.Results.ForEach(Contacts.Add);
+            }
+            catch (ApiException apiException)
+            {
+                Console.WriteLine(apiException.Message);
+                Debugger.Break();
+            }
+
         }
     }
 }
